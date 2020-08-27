3e382ba0c5b94e265dc03cc47dd2721e4fd7d282
Megha Agarwal <meghaa105@gmail.com>
2020-08-27 23:19:59 +0530

    kickstarter

---
+++ Test/tempCodeRunnerFile.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+// The first line of the input gives the number of test cases, T. T test cases follow. Each test case starts with a line containing the two integers N and P, the number of students and the number of students you need to pick, respectively. Then, another line follows containing N integers Si; the i-th of these is the skill of the i-th student. 
+int main()
+{
+    int n,total_stu,team_final,temp;
+    vector<int> team;
+    cin >> n;
+    while(n--)
+    {
+        // cout << "Enter the total size" << endl;
+        cin >> total_stu >> team_final;
+        for(int i = 0;i<total_stu;i++)
+        {
+            cin >> temp;
+            team.push_back(temp);
+        }
+        sort(team.begin(),team.end());
+        int min = INT_MAX;
+        int dif = 0;
+        int k,i;
+        for(i=0;i<=total_stu-team_final;i++)
+        {
+            dif = 0;
+            k = i+team_final- 1;
+            cout << k << endl;
+            temp = k - 1;
+            while(temp>=i && temp >=0)
+            {
+                dif = dif + (team[k]-team[temp]);
+                temp--;
+            }            
+            if(dif < min)
+            {
+                min = dif;
+            }            
+        }
+        cout << min << endl;
+        team.clear();
+    }
+
+    return 0;
+
+}
